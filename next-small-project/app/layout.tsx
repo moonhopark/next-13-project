08215c1a6ea4878611006a5d4c311b78c6a01c4b
moonhopark <moonho4199@naver.com>
2023-09-06 15:05:32 +0900

    feat: wiki rocket project setting

---
+++ next-small-project/app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'WikiRocket!',
+  description: 'next 13 small project',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-slate-800`}>
+        <Navbar />
+        {children}
+      </body>
+    </html>
+  );
+}
