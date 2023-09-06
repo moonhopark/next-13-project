980f5bfc81af0d705597ee55bf8df296e239f0f3
moonhopark <moonho4199@naver.com>
2023-09-06 17:00:38 +0900

    feat: blog project setting

---
+++ next-blog-website/app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
+import MyProfilePic from './components/MyProfilePic';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Moonho blog',
+  description: 'created by Moonho',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-slate-800 text-white`}>
+        <Navbar />
+        <MyProfilePic />
+        {children}
+      </body>
+    </html>
+  );
+}
